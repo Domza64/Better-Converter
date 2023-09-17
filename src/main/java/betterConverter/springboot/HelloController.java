effd76b3faa048d778403415ee7a0702f02a4c17
Domza64 <juricicdominik@protonmail.com>
2023-09-17 22:06:33 +0200

    Client can now download files

--- src/main/java/betterConverter/springboot/HelloController.java
+++ src/main/java/betterConverter/springboot/HelloController.java
@@ -7,15 +7,21 @@ import com.github.kiulian.downloader.downloader.response.Response;
 import com.github.kiulian.downloader.model.videos.VideoInfo;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
+    private List<Song> downloadedSongs = new ArrayList<>(); // TODO - Replace with h2 database
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("linkDto", new LinkDto());
@@ -33,31 +39,60 @@ public class HelloController {
             and client is redirected
         */
         downloadSong(linkDto.getLinkString(), hashCode);
-        return "redirect:/download?id=" + hashCode;
+        return "redirect:/?download=" + hashCode;
     }
 
     // TODO - Song object, has download hash id thing for download and file name and byte thing for storing actual file
 
-    @GetMapping("/download")
-    public StreamingResponseBody downloadFile(HttpServletResponse response, @RequestParam(required = false) String id) {
+//    @GetMapping("/download")
+//    public StreamingResponseBody downloadFile(HttpServletResponse response, @RequestParam(required = false) String id) {
+//        if (id == null) return null;
+//
+//        Song song = null;
+//        for (Song s : downloadedSongs) {
+//            if (String.valueOf(s.getId()).equals(id)) {
+//                song = s;
+//            }
+//        }
+//        if (song == null) return null; // TODO - Redirect to index
+//
+////        File file = new File(System.getProperty("user.dir") + "/tempDownloads/" + id + ".m4a");
+//
+//        File songFile = song.getData();
+//
+//        response.setContentType(song.getContentType());
+//        response.setHeader(
+//                HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + song.getTitle() + "\"");
+//
+//        return outputStream -> {
+//            int bytesRead;
+//            byte[] buffer = new byte[100];
+//            try (InputStream inputStream = file.toURI().toURL().openStream()) {
+//                while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                    outputStream.write(buffer, 0, bytesRead);
+//                }
+//            }
+//        };
+//    }
 
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadSong(@RequestParam(required = false) String id) {
         if (id == null) return null;
 
-        File file = new File(System.getProperty("user.dir") + "/tempDownloads/" + id + ".m4a");
+        System.out.println(downloadedSongs);
 
-        response.setContentType("m4a");
-        response.setHeader(
-                HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + id + "\"");
-
-        return outputStream -> {
-            int bytesRead;
-            byte[] buffer = new byte[100];
-            try (InputStream inputStream = file.toURI().toURL().openStream()) {
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
+        Song song = null;
+        for (Song s : downloadedSongs) {
+            if (String.valueOf(s.getId()).equals(id)) {
+                song = s;
             }
-        };
+        }
+        if (song == null) return null; // TODO - Redirect to index
+
+        System.out.println("hmhm");
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + song.getTitle() + "\"")
+                .body(song.getData());
     }
 
     private void downloadSong(String linkString, int hashCode) {
@@ -75,7 +110,16 @@ public class HelloController {
                 .renameTo(String.valueOf(hashCode)) // video.details().title()
                 .overwriteIfExists(true); // if false and file with such name already exits sufix will be added video(1).mp4
         Response<File> responsee = downloader.downloadVideoFile(requestt);
-        File data = responsee.data();
+        try {
+            String extension = "." + video.audioFormats().get(0).extension().value();
+            File data = new File(System.getProperty("user.dir") + "/tempDownloads/" + hashCode + extension);
+            Song song = new Song(video.details().title(), hashCode, extension, Files.readAllBytes(data.toPath()));
+            downloadedSongs.add(song);
+            // Delete song from filesistem
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Err");
+        }
     }
 
     private String getIdFromLink(String linkString) {
